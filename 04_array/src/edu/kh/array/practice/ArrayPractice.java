af56a32ec9726844ca52b0b2c28e9c2336e18908
DESKTOP-Q6S4J5T\user1 <mj3189@naver.com>
2024-01-31 17:47:12 +0900

    20240131_ 자바 수업

---
+++ 04_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -0,0 +1,37 @@
+package edu.kh.array.practice;
+
+import java.util.Scanner;
+
+public class ArrayPractice {
+
+	
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public void practice1() {
+		// 길이가 9인 배열을 선언 및 할당하고,
+		int[] arr = new int[9];
+		
+		//	1부터 9까지의 값을 반복문을 이용하여
+		//	순서대로 배열의 각 인덱스 요소에 대입
+		for(int i=0 ; i < arr.length ; i++) {
+			arr[i] = i+1;
+		}
+		System.out.print( [i] + " "  );
+		arr[i] = sc.nextInt();
+		
+		System.out.println();
+		
+		
+		
+	}
+	
+	
+	
+	public void practice2() {
+		
+		int[] arr = new int[9];
+		
+		
+	}
+}
---
+++ 04_array/src/edu/kh/array/practice/PracticeRun.java
@@ -0,0 +1,14 @@
+package edu.kh.array.practice;
+
+public class PracticeRun {
+
+	public static void main(String[] args) {
+		
+		ArrayPractice ap = new ArrayPractice();
+		
+		ap.practice1();
+		
+		
+		
+	}
+}
